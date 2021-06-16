86ead848d9e1933a655e2fc167247d472deaac52
Ivo <ab691072@ens.uqam.ca>
2021-06-16 17:05:54 -0400

    Second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import classes from "./header.module.css";
+import { Fragment } from "react";
+import headerMealImage from '../../assets/meals.jpg';
+
+
+/**
+ * React Component representing an HTML header and img element
+ * @returns Header
+ */
+const Header = () => {
+  return <Fragment>
+    <header className={classes.header}>
+      <h1>ReactMeals</h1>
+      <button>Cart</button>
+    </header>
+    <div className={classes["main-image"]}>
+      <img src={headerMealImage} alt="Table of food"/>
+    </div>
+  </Fragment>;
+};
+
+export default Header;
