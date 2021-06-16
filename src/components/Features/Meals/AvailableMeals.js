a909b7f52ae0bcc6b3b5790631a72051b43704b3
Ivo <ab691072@ens.uqam.ca>
2021-06-16 17:46:03 -0400

    Added code for AvailableMeals.js

--- src/components/Features/Meals/AvailableMeals.js
+++ src/components/Features/Meals/AvailableMeals.js
@@ -1,5 +1,13 @@
-import DUMMYY_DB from './dummy_db'
+import DUMMY_DB from './dummy_db'
+import classes from './AvailableMeals.module.css'
 
-const AvailableMeals = () => {};
+const AvailableMeals = () => {
+  const mealsList = DUMMY_DB.map(element => <li>{element.name}</li>);
+  return <section className={classes.meals}>
+    <ul>
+      {mealsList}
+    </ul>
+  </section>
+};
 
 export default AvailableMeals;
