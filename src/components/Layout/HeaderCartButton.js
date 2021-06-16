b1cc40f0e4ee56ba12ff1a8d2e5dfa7199ad59d0
Ivo <ab691072@ens.uqam.ca>
2021-06-16 17:19:59 -0400

    Added css classes to HeaderCartButton.js

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,12 +2,12 @@ import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Features/Cart/CartIcon'
 
 export default HeaderCartButton = props => {
-  return <button>
-    <span>
+  return <button className={classes.button}>
+    <span className={classes.icon}>
       <CartIcon/>
     </span>
     <span>Your Cart</span>
-    <span>
+    <span className={classes.badge}>
       {/* Number of items in cart */}
     </span>
   </button>
